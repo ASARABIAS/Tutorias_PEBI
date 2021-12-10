0b482ce78a222d920d8c471dc7c511af74e873ae
msgaray <90914937+msgaray@users.noreply.github.com>
2021-12-09 21:44:35 -0500

    nueva tutoria terminado

--- Tools/Backend/load_administrator.php
+++ Tools/Backend/load_administrator.php
@@ -12,15 +12,23 @@ if(isset($_SESSION['usuario'])){
    $nombrec=$_POST['name_completo'];
    $emailc=$_POST['email_completo'];
    $passwordc=$_POST['password_completo'];
+   $nombret=$_POST['name_tutoring'];
+   $id_docentet=$_POST['docentes'];
+   $id_courset=$_POST['asig_tutoring'];
+   $descriptiont=$_POST['description'];
+   $opcionh=$_POST['opcion_horario'];
+   $link=$_POST['link'];
+   $salon=$_POST['salon'];
 
+   
    if(isset($name_career)){
     $sql = "INSERT INTO career(id,name,id_administrator) VALUES (default,'$name_career', '$id_login')";
         
     if (mysqli_query($conn, $sql)) {
           header("location:../../administrator.php");
-        } else {
+    } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        } 
+      } 
     }else if(isset($name_course)){ 
             $sq = "INSERT INTO course(id,name,id_career) VALUES (default,'$name_course', '$id_carrera')";
             if (mysqli_query($conn, $sq)) {
@@ -45,6 +53,7 @@ if(isset($_SESSION['usuario'])){
         } else {
                 echo "Error: " . $sq . "<br>" . mysqli_error($conn);
         } 
+      
       }else if($rol=="estudiante"){
         $sq = "INSERT INTO user(id,email,password) VALUES (default,'$emailc', '$passwordc')"; 
         if (mysqli_query($conn, $sq)) { 
@@ -60,10 +69,27 @@ if(isset($_SESSION['usuario'])){
           } else {
                   echo "Error: " . $sq . "<br>" . mysqli_error($conn);
           }
-      }        
+      }
+      }else if(isset($nombret)){
+        echo "entro";
+        $sql = "INSERT INTO tutoring(id,name,description,id_teacher,id_course) VALUES (default,'$nombret','$descriptiont', '$id_docentet','$id_courset')";   
+          if (mysqli_query($conn, $sql)) {
+            header("location:../../administrator.php");
+              } else {
+                      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+              } 
+                $us= $conn->query("SELECT * FROM tutoring WHERE `name` LIKE '$nombret' ");
+               $idt = $us->fetch_assoc()['id'];
+               $sq= "INSERT INTO schedule(id,opening_date,closing_date,url_calendar,salon,id_tutoring) VALUES (default,NOW(),'null', '$link','null','$idt')";   
+             if (mysqli_query($conn, $sq)) {
+                  header("location:../../administrator.php");
+                } else {
+                         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                 } 
+      }  
+}    
   
-}
 
 
-} 
+
 mysqli_close($conn);
--- Tools/administrator/new_tutoring.php
+++ Tools/administrator/new_tutoring.php
@@ -1,68 +1,116 @@
 
    <div class="tutorial_container">
  
- <form action="/my-handling-form-page" method="POST">
+      <form action="Tools/Backend/load_administrator.php" method="POST">
        <ul>
        <li>
        <h3>INFORMACION DE LA TUTORIA </h3>
        </li>
        <li>
        <label class="labelre">Nombre: </label>
-     <input type="text" name="nombre" required>
+     <input type="text" name="name_tutoring" style="text-transform:uppercase;"   onkeyup="javascript:this.value=this.value.toUpperCase();" required>
      </li>
        <li>
        <label class="labelre">Programa:</label>
-     <select name="programas" >
-         <option> Ingenieria de sistemas</option>
-         <option> Ingenieria ambiental y sanitaria</option>
-         <option> Ingenieria agroindustrial</option>
-         <option> Contaduria publica</option>
-         <option> Adminstracion de empresas</option>
-         <option> Tecnología Agropecuaria</option>
-         <option> Economia</option>
-       </select>
+       <select name="programa_tutoring" id="lista1" >
+                            <option value="0">Seleccione:</option>
+                            <?php   
+                            if(!isset($conn)){
+                                session_start();
+                                require_once '../Backend/conection.php';
+                            }      
+                            if (isset($_SESSION['usuario'])) {
+                                 $id=$_SESSION['id_user'];
+                                $result = $conn->query("SELECT * FROM `career` WHERE `id_administrator` LIKE '$id' ");
+                                var_dump($result);
+                                 while ($fila=$result->fetch_assoc()) {
+                                    $idc=$fila['id'];
+                                    $namec=$fila['name'];
+                                 ?>
+                                   <option  value="<?php echo($idc); ?>"> <?php echo($namec); ?></option>
+                                  <?php 
+                                 }
+                               
+                            }
+                            ?>
+                          </select>
+       </li>
+       <li>
+       <label class="labelre">Asignatura:</label>
+       <div id="select2lista"></div>
        </li>
        <li>
        <label class="labelre"> Descripcion temas:</label>
-       <textarea name="texto" placeholder="Descripcion"></textarea>
+       <textarea name="description" placeholder="Descripcion" style="text-transform:uppercase;"   onkeyup="javascript:this.value=this.value.toUpperCase();" ></textarea>
        </li>
        <li>
        <label class="labelre"> Tutor asignado:</label>
-     <select name="programas" >
-         <option> Miguel rincon</option>
-         <option> darwin navarro</option>
-         <option> jhon timi lopez</option>
-       </select>
+       <select name="docentes" >
+                            <option value="0">Seleccione:</option>
+                            <?php   
+                            if(!isset($conn)){
+                                session_start();
+                                require_once '../Backend/conection.php';
+                            }      
+                            if (isset($_SESSION['usuario'])) {
+                                
+                                $result = $conn->query("SELECT * FROM `teacher`");
+                                var_dump($result);
+                                 while ($fila=$result->fetch_assoc()) {
+                                    $idc=$fila['id'];
+                                    $namec=$fila['name'];
+                                 ?>
+                                   <option  value="<?php echo($idc); ?>"> <?php echo($namec); ?></option>
+                                  <?php 
+                                 }    
+                            }
+                            ?>
+
+                          </select>
        </li>
        <li>
        <label > Horario:</label>
-       <input type="radio"  class="selected" name="presencial" value="presencial" required> presencial
-       <input type="radio" class="selected"  name="virtual" value="virtual" required> virtual 
+       <input type="radio"  class="selected" name="opcion_horario" value="presencial" required> presencial
+       <input type="radio" class="selected"  name="opcion_horario" value="virtual" required> virtual 
        </li>      
        <li>
        <label></label>  
-       <a href="https://calendar.google.com/calendar/u/0/r/settings/createcalendar?cid=cjkyOWhjZzF0MHFpdDNjbmM5Nzg3cGJraWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&pli=1">Click aqui! , Crea el horario en Google calendar y captura el link</a>
+       <a target="_blank" href="https://calendar.google.com/calendar/u/0/r/settings/createcalendar?cid=cjkyOWhjZzF0MHFpdDNjbmM5Nzg3cGJraWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&pli=1">Click aqui! , Crea el horario en Google calendar y captura el link</a>
       </li>
        <li>
        <label class="labelre"> Link tutoria: </label>
-     <input type="text" name="link" required>
+      <input type="text"  name="link" style="text-transform:uppercase;"   onkeyup="javascript:this.value=this.value.toUpperCase();" required>
      </li>
      <li>
      <label class="labelre"> Salon: </label>
-     <input type="text" name="salon" required>
+     <input type="number" name="salon"  >
      </li>
      <li>
      <label> </label>
-     <button  type="reset" class="boton">Crear tutoria</button>
+     <button  type="submit" value="Enviar" class="boton">Crear tutoria</button>
      </li>
        </ul>
        </form>
-       
-   <div class="tutorial_container_coumn_1">
-  <div class="special">
-   <form class="login-form" >
-   <div class="row">
-       <div src="" class="dropimage">
-               <input title="HacerClick aqui" type="file" class="foto" name="foto1" id="foto1" required="true">
-       </div> 
    </div> 
+   <script type="text/javascript">
+	$(document).ready(function(){
+		$('#lista1').val(1);
+		recargarLista();
+
+		$('#lista1').change(function(){
+			recargarLista();
+		});
+	})
+</script>
+<script type="text/javascript">
+	function recargarLista(){
+		$.ajax({
+			type:"POST",
+			url:"Tools/Backend/cargaroptions_administrator.php",
+			data:"carrera=" + $('#lista1').val(),
+			success:function(r){
+				$('#select2lista').html(r);
+			}
+		});
+	}
+</script>
